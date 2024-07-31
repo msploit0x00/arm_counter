c34f81f892d36f7e721657defaa70d9fad456a42
msploit0x00 <minasploit1@outlook.com>
2024-07-31 13:49:56 +0300

    fixing trigger to be before_save

--- arm_counter/counter.py
+++ arm_counter/counter.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class CustomSalesInvoice(SalesInvoice):
-    def on_submit(self):
+    def before_save(self):
         counter = 0
         if self.pos_profile == 'Casher 4 Armsha':
             # sales_list = frappe.get_list('Sales Invoice',fields=['name'],filters={'posting_date': self.posting_date,'pos_profile':'Casher 4 Armsha'})
