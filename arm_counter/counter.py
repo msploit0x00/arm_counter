20faeae7a37d63b6d7c2c4eaed25148eae3d091e
msploit0x00 <minasploit1@outlook.com>
2024-07-31 18:23:39 +0300

    new logic

--- arm_counter/counter.py
+++ arm_counter/counter.py
@@ -4,30 +4,64 @@ from datetime import datetime
 
 
 
+
+
 class CustomSalesInvoice(SalesInvoice):
     def on_submit(self):
-        # frappe.msgprint("HERE")
-        # counter = 0
-        # if self.pos_profile == 'Casher 4 Armsha':
+        # Ensure the counter is set to 0 initially
         counter = 0
-            # sales_list = frappe.get_list('Sales Invoice',fields=['name'],filters={'posting_date': self.posting_date,'pos_profile':'Casher 4 Armsha'})
-            # sales_list = frappe.db.get_list('Sales Invoice',{'pos_profile':'Casher 4 Armsha', 'posting_date': datetime.now().date()})
-                # count = 0
-        invoices = frappe.get_list(
-                "Sales Invoice",
-                filters={"posting_date": self.posting_date,'pos_profile':'Casher 4 Armsha'},
-                fields=["name"],
-                limit_start=0,
-                limit_page_length=5000,
-            )
 
-        # if invoices:
+        # Fetch the list of invoices for the given date and POS profile
+        invoices = frappe.get_list(
+            "Sales Invoice",
+            filters={"posting_date": self.posting_date, 'pos_profile': 'Casher 4 Armsha'},
+            fields=["name"],
+            limit_start=0,
+            limit_page_length=5000,
+        )
+
+        # Count the number of invoices
         counter = len(invoices)
-            # counter = sales_list + 1
+
+        # Set the custom_daily_counter2 field
         self.custom_daily_counter2 = counter + 1
+
+        # Save the document to the database
+        self.save(ignore_permissions=True)
+
+        # Ensure the missing values are set and flags are correctly configured
         self.set_missing_values()
         self.flags.ignore_permissions = True
         frappe.flags.ignore_account_permission = True
-            # frappe.msgprint("Done")
-            # frappe.db.set_value('Sales Invoice',self.name,self.custom_daily_counter2,counter)
+
+        # Commit the changes to the database
+        frappe.db.commit()
+
+
+# class CustomSalesInvoice(SalesInvoice):
+#     def on_submit(self):
+#         # frappe.msgprint("HERE")
+#         # counter = 0
+#         # if self.pos_profile == 'Casher 4 Armsha':
+#         counter = 0
+#             # sales_list = frappe.get_list('Sales Invoice',fields=['name'],filters={'posting_date': self.posting_date,'pos_profile':'Casher 4 Armsha'})
+#             # sales_list = frappe.db.get_list('Sales Invoice',{'pos_profile':'Casher 4 Armsha', 'posting_date': datetime.now().date()})
+#                 # count = 0
+#         invoices = frappe.get_list(
+#                 "Sales Invoice",
+#                 filters={"posting_date": self.posting_date,'pos_profile':'Casher 4 Armsha'},
+#                 fields=["name"],
+#                 limit_start=0,
+#                 limit_page_length=5000,
+#             )
+
+#         # if invoices:
+#         counter = len(invoices)
+#             # counter = sales_list + 1
+#         self.custom_daily_counter2 = counter + 1
+#         self.set_missing_values()
+#         self.flags.ignore_permissions = True
+#         frappe.flags.ignore_account_permission = True
+#             # frappe.msgprint("Done")
+#             # frappe.db.set_value('Sales Invoice',self.name,self.custom_daily_counter2,counter)
 
