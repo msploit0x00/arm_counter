95cc99b560e3c56d2ab1a1918ed2fc05df37e09f
msploit0x00 <minasploit1@outlook.com>
2024-07-31 14:05:00 +0300

    change trigger

--- arm_counter/counter.py
+++ arm_counter/counter.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class CustomSalesInvoice(SalesInvoice):
-    def before_save(self):
+    def validate(self):
         counter = 0
         if self.pos_profile == 'Casher 4 Armsha':
             # sales_list = frappe.get_list('Sales Invoice',fields=['name'],filters={'posting_date': self.posting_date,'pos_profile':'Casher 4 Armsha'})
