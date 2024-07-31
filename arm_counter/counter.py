570ff39cfbb12551bdce4f9af47a463983b483cd
msploit0x00 <minasploit1@outlook.com>
2024-07-31 14:16:45 +0300

    fixing counter variable

--- arm_counter/counter.py
+++ arm_counter/counter.py
@@ -6,8 +6,9 @@ from datetime import datetime
 
 class CustomSalesInvoice(SalesInvoice):
     def on_submit(self):
-        counter = 0
+        # counter = 0
         if self.pos_profile == 'Casher 4 Armsha':
+            counter = 1
             # sales_list = frappe.get_list('Sales Invoice',fields=['name'],filters={'posting_date': self.posting_date,'pos_profile':'Casher 4 Armsha'})
             sales_list = frappe.db.count('Sales Invoice',{'pos_profile':'Casher 4 Armsha', 'posting_date': datetime.now().date()})
             counter = sales_list + 1
