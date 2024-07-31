5a5f890d8c8775a14d034340b1b6476f6d1de90c
msploit0x00 <minasploit1@outlook.com>
2024-07-31 14:07:19 +0300

    change to on_submit

--- arm_counter/counter.py
+++ arm_counter/counter.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class CustomSalesInvoice(SalesInvoice):
-    def validate(self):
+    def on_submit(self):
         counter = 0
         if self.pos_profile == 'Casher 4 Armsha':
             # sales_list = frappe.get_list('Sales Invoice',fields=['name'],filters={'posting_date': self.posting_date,'pos_profile':'Casher 4 Armsha'})
